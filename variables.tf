1d0ca6909263a4d48df64e21efd562721ac7f8b5
Nico TSESSILEDE <31391553+nicotsessilede@users.noreply.github.com>
2022-05-15 04:14:23 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,22 @@
 variable "name" {
   type        = string
   description = "The name of the Application Gateway."
+  default     = "myAppGateway"
 }
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group in which to create the Application Gateway."
+  default     = "myResourceGroupAG"
 }
 variable "location" {
   type        = string
   description = "The location/region where the Application Gateway is created."
+  default     = "eastus"
 }
 variable "sku" {
   type        = map(string)
   description = "A mapping with the sku configuration of the application gateway."
-  # sku = { tier = "", size = "", capacity = "" }
+   sku = { tier = "Standard_v2", size = "2", capacity = "Standard_v2" }
 }
 variable "autoscale_configuration" {
   type        = map(string)
@@ -24,6 +27,7 @@ variable "autoscale_configuration" {
 variable "subnet_id" {
   type        = string
   description = "The ID of the Subnet which the Application Gateway should be connected to."
+  default     = "myBackendSubnet"
 }
 variable "waf_configuration" {
   type        = map(string)
@@ -34,12 +38,12 @@ variable "waf_configuration" {
 variable "frontend_ip_configuration" {
   type        = map(string)
   description = "A mapping the front ip configuration."
-  # frontend_ip_configuration = { public_ip_address_id = "", private_ip_address = "", private_ip_address_allocation = "" }
+   frontend_ip_configuration = { public_ip_address_id = "", private_ip_address = "", private_ip_address_allocation = "" }
 }
 variable "backend_address_pools" {
   type        = list(map(string))
   description = "List of objects that represent the configuration of each backend address pool."
-  # backend_address_pools = [{ name = "", ip_addresses = "" }]
+  backend_address_pools = [{ name = "", ip_addresses = "" }]
 }
 variable "identity_id" {
   type        = string
@@ -55,23 +59,23 @@ variable "ssl_certificates" {
 variable "http_listeners" {
   type        = list(map(string))
   description = "List of objects that represent the configuration of each http listener."
-  # http_listeners = [{ name = "", frontend_ip_configuration = "", port = "", protocol = "", host_name = "", ssl_certificate_name = "" }]
+  http_listeners = [{ name = "", frontend_ip_configuration = "", port = "", protocol = ""}]
 }
 variable "probes" {
   type        = list(map(string))
   default     = []
   description = "List of objects that represent the configuration of each probe."
-  # probes = [{ name = "", host = "", protocol = "", path = "", interval = "", timeout = "", unhealthy_threshold = "" }]
+  probes = [{ name = "", host = "", protocol = "", path = "", interval = "", timeout = "", unhealthy_threshold = "" }]
 }
 variable "backend_http_settings" {
   type        = list(map(string))
   description = "List of objects that represent the configuration of each backend http settings."
-  # backend_http_settings = [{ name = "", port = "", protocol = "", request_timeout = "", host_name = "", probe_name = "" }]
+  backend_http_settings = [{ name = "", port = "", protocol = "", request_timeout = "", host_name = "", probe_name = "" }]
 }
 variable "request_routing_rules" {
   type        = list(map(string))
   description = "List of objects that represent the configuration of each backend request routing rule."
-  # request_routing_rules = [{ name = "", http_listener_name = "", backend_address_pool_name = "", backend_http_settings_name = "" }]
+   request_routing_rules = [{ name = "", http_listener_name = "", backend_address_pool_name = "", backend_http_settings_name = "" }]
 }
 variable "tags" {
   type        = map(string)
